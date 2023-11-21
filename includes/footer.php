4c66931a7676662b75967a170df00e19df4c74dc
Leonardo de Souza <contato@leonardosouza.com.br>
2023-11-21 14:16:15 -0300

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -3,32 +3,32 @@
         <div class="col-3">
             <span>Sobre</span>
             <ul>
-                <a href="quem-somos.php" target="_blank"><li>Quem somos</li></a>
-                <a href="servicos.php" target="_blank"><li>Serviços</li></a>
-                <a href="recursos.php" target="_blank"><li>Recursos</li></a>
-                <a href="perguntas-frequentes.php" target="_blank"><li>Perguntas Frequentes</li></a>
-                <a href="termos-e-condicoes.php" target="_blank"><li>Termos e Condições</li></a>
+                <a href="quem-somos" target="_blank"><li>Quem somos</li></a>
+                <a href="servicos" target="_blank"><li>Serviços</li></a>
+                <a href="recursos" target="_blank"><li>Recursos</li></a>
+                <a href="perguntas-frequentes" target="_blank"><li>Perguntas Frequentes</li></a>
+                <a href="termos-e-condicoes" target="_blank"><li>Termos e Condições</li></a>
             </ul>
         </div>
 
         <div class="col-3">
             <span>Aprendizado</span>
             <ul>
-                <a href="ajuda.php" target="_blank"><li>Ajuda</li></a>
-                <a href="dicas-para-negocios.php" target="_blank"><li>Dicas para negócios</li></a>
-                <a href="servicos-locais.php" target="_blank"><li>Serviços Locais</li></a>
-                <a href="depoimentos.php" target="_blank"><li>Depoimentos</li></a>
+                <a href="ajuda" target="_blank"><li>Ajuda</li></a>
+                <a href="dicas-para-negocios" target="_blank"><li>Dicas para negócios</li></a>
+                <a href="servicos-locais" target="_blank"><li>Serviços Locais</li></a>
+                <a href="depoimentos" target="_blank"><li>Depoimentos</li></a>
             </ul>
         </div>
 
         <div class="col-3">
             <span>Dicas</span>
             <ul>
-                <a href="dicas/restaurantes.php" target="_blank"><li>Restaurantes</li></a>
-                <a href="dicas/saloes-de-beleza.php" target="_blank"><li>Salões de Beleza</li></a>
-                <a href="dicas/mecanicas.php" target="_blank"><li>Mecânicas</li></a>
-                <a href="dicas/dentistas.php" target="_blank"><li>Dentistas</li></a>
-                <a href="dicas/moveis.php" target="_blank"><li>Móveis</li></a>
+                <a href="dicas/restaurantes" target="_blank"><li>Restaurantes</li></a>
+                <a href="dicas/saloes-de-beleza" target="_blank"><li>Salões de Beleza</li></a>
+                <a href="dicas/mecanicas" target="_blank"><li>Mecânicas</li></a>
+                <a href="dicas/dentistas" target="_blank"><li>Dentistas</li></a>
+                <a href="dicas/moveis" target="_blank"><li>Móveis</li></a>
             </ul>
         </div>
 
